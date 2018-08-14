f91725e3c94d1dd3cac3a5d6879e36f9f823f746
JoJo_WEN <m18670825965@163.com>
2018-08-14 19:03:23 +0800

    [web] add init

---
+++ pokemon-web/src/color.js
@@ -0,0 +1,13 @@
+let colors = [{
+  name: '水箭龟',
+  color: '#585fdb',
+  rgb: 'rgb(88, 95, 219)',
+}, {
+  name: '皮卡丘',
+  color: '#fde069',
+  rgb: 'rgb(253, 224, 105)',
+}, {
+  name: '伊布',
+  color: '#fcc335',
+  rgb: 'rgb(88, 95, 219)',
+}];
